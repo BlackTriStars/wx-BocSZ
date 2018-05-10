9f7e0a678753096ac0bd32193c26374360345fac
Liang Bing <bing_liang@17english.com>
2018-05-10 12:35:36 +0800

    lb

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.config.productionTip = false
 import 'lib-flexible';
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper);
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,27 @@
         <div class="searchWrap">
           <search @afterInput="search"></search>
         </div>
+        <swiper style="height: 100%" :options="swiperOption">
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../assets/index/banner.png" alt="">
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
       </div>
       <div class="navList">
         <ul>
@@ -17,17 +38,17 @@
     </div>
     <section>
       <ul>
-        <li class="main" v-for="(main,index) in mainList" :key="index">
-          <div class="topLine">
-            <div class="title">
+        <li class="main clear" v-for="(main,index) in mainList" :key="index">
+          <div class="topLine clear">
+            <div class="title left" :style="{color:main.topLine.color}">
               <img class="icon" :src="require('../assets/index/' + main.topLine.img + '.png')" alt="">
               {{main.topLine.title}}
             </div>
-            <span class="more">
-              查看更多 >
+            <span class="more right">
+              查看更多 <i class="icon-more"></i>
             </span>
           </div>
-          <div class="mainItem" v-for="(item,idx) in main.main" :key="idx">
+          <div class="mainItem left" v-for="(item,idx) in main.main" :key="idx">
             <div class="item">
               <img :src="require('../assets/index/' + item.img + '.png')" alt="">
               <p class="desc">{{item.content}}</p>
@@ -66,7 +87,8 @@ export default {
         {
           topLine:{
             img: 'title-new',
-            title: '最新课程'
+            title: '最新课程',
+            color: '#0c6acc'
           },
           main:[
             {
@@ -81,7 +103,8 @@ export default {
         {
           topLine:{
             img: 'title-hot',
-            title: '热门推荐'
+            title: '热门推荐',
+            color: '#f0324a'
           },
           main:[
             {
@@ -96,7 +119,8 @@ export default {
         {
           topLine:{
             img: 'title-information',
-            title: '培训动态'
+            title: '培训动态',
+            color: '#09c6bb'
           },
           main:[
             {
@@ -111,7 +135,8 @@ export default {
         {
           topLine:{
             img: 'title-history',
-            title: '往期回顾'
+            title: '往期回顾',
+            color: '#ff5025'
           },
           main:[
             {
@@ -123,7 +148,12 @@ export default {
             },
           ]
         },
-      ]
+      ],
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
     }
   },
   components:{
@@ -150,6 +180,25 @@ export default {
           padding: 34px 30px 0;
           top: 0;
           left: 0;
+          z-index: 999;
+        }
+        .swiper-pagination{
+          position: absolute;
+          display: inline-block;
+          bottom: 30px;
+          right: 30px;
+          width: auto!important;
+          left: auto!important;
+          .swiper-pagination-bullet{
+            width: 26px;
+            height: 26px;
+            &+.swiper-pagination-bullet{
+              margin-left: 18px;
+            }
+            &.swiper-pagination-bullet-active{
+              background: #91d9ff;
+            }
+          }
         }
       }
       .navList{
@@ -171,11 +220,63 @@ export default {
             align-items: center;
             padding: 30px 0 26px;
             font-size: 32px;
+            color: #333;
             img{
               width: 188px;
               height: 188px;
               display: block;
               margin-bottom: 16px;
+            }
+          }
+        }
+      }
+    }
+    section{
+      margin-top: 30px;
+      border-top: 1px solid #999; /*no*/
+      background: #fff;
+      width: 100%;
+      padding-top: 10px;
+      .main{
+        border-bottom: 1px solid #dcdcdc;/*no*/
+        padding: 0 30px 44px;
+        
+        .topLine{
+          height: 120px;
+          width: 100%;
+          line-height: 120px;
+          .title{
+            font-size: 48px;
+            font-weight: bold;
+            img{
+              width: 60px;
+              height: 60px;
+              display: inline-block;
+              transform: translateY(10px);
+            }
+
+          }
+          .more{
+            font-size: 32px;
+            color: #999;
+          }
+        }
+        .mainItem{
+          &:nth-child(2n-1){
+            float: right;
+          }
+          .item{
+            width: 490px;
+            border: 1px solid #dcdcdc;/*no*/
+            img{
+              width: 100%;
+              display: block;
+            }
+            p{
+              font-size: 36px;
+              color: #333;
+              line-height: 80px;
+              text-align: center;
             }
           }
         }
