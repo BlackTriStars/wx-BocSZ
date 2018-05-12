a5713883467f7fbe436c824992d67bce401c8e86
IllidanJ <393503475@qq.com>
2018-05-13 01:01:11 +0800

    + material

--- src/App.vue
+++ src/App.vue
@@ -7,73 +7,76 @@
 </template>
 
 <script>
-import defaultLayout from '@/components/layout/default.vue'
-import { mapMutations } from 'vuex'
+import defaultLayout from "@/components/layout/default.vue";
+import { mapMutations } from "vuex";
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
       headerShow: false,
       footerShow: false,
-      headerBgColor: '#0181ca'
-    }
+      headerBgColor: "#0181ca"
+    };
   },
-  components:{
+  components: {
     defaultLayout
   },
-  mounted () {
-    this.checkoutPath(this.$route.name)
+  mounted() {
+    this.checkoutPath(this.$route.name);
   },
-  methods:{
-    ...mapMutations(['changeHeader']),
+  methods: {
+    ...mapMutations(["changeHeader"]),
     // 根据当前url 给header设置不同颜色
     //  默认的颜色不用设置
-    checkoutPath(name){
-      if(name === 'index'){
+    checkoutPath(name) {
+      if (name === "index") {
         this.headerShow = false;
         this.footerShow = true;
-      }else{
-        this.headerShow = true;        
-        this.footerShow = false
+      } else {
+        this.headerShow = true;
+        this.footerShow = false;
       }
+      console.log(name);
       switch (name) {
-        case 'user':
-          this.changeHeader({title:'个人中心'})
+        case "user":
+          this.changeHeader({ title: "个人中心" });
           break;
-        case 'collection':
-          this.changeHeader({title:'培训资料',color:'#db3f40'})
+        case "collection":
+          this.changeHeader({ title: "培训资料", color: "#db3f40" });
           break;
-        case 'integral':
-          this.changeHeader({title:'我的积分'})
+        case "integral":
+          this.changeHeader({ title: "我的积分" });
           break;
-        case 'myStudy':
-          this.changeHeader({title:'我的学习'})
+        case "myStudy":
+          this.changeHeader({ title: "我的学习" });
           break;
-        case 'survey':
-          this.changeHeader({title:'问卷调研'})
+        case "survey":
+          this.changeHeader({ title: "问卷调研" });
+          break;
+        case "material":
+          this.changeHeader({ title: "培训资料" });
           break;
         default:
           break;
       }
     }
   },
-  watch:{
-    $route(v){
-      console.log(v)
-      this.checkoutPath(v.name)
+  watch: {
+    $route(v) {
+      console.log(v);
+      this.checkoutPath(v.name);
     }
   }
-}
+};
 </script>
 
 <style>
-@import './styles/fonts/style.css';
+@import "./styles/fonts/style.css";
 html,
 body,
 #app {
   width: 100%;
   height: 100%;
   background: #ececec;
-  
 }
 </style>
