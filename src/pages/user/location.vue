f0ca61d6a95ae9e2111f1b7583574859a8c0832f
Liang Bing <bing_liang@17english.com>
2018-05-29 20:36:46 +0800

    lb

--- src/pages/user/location.vue
+++ src/pages/user/location.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="location">
     <div class="banner">
+      <img :src="{bannerImg}" alt="">
     </div>
     <div class="introduce">
-
+      {{introduce}}
     </div>
-    <div class="loc"></div>
-    <div class="traffic">
-      <div class="title"></div>
-      <div class="trafficBtn">
-        <div class="btn"></div>
-        <div class="btn"></div>
-      </div>
-      <div class="map"></div>
+    <div class="loc">
+      <img :src="{locMap}" alt="">
     </div>
-    <div class="traffic">
-      <div class="title"></div>
+    <div class="traffic" v-for="(item, index) in traffic" :key="index">
+      <div class="title">{{item.title}}</div>
       <div class="trafficBtn">
-        <div class="btn"></div>
-        <div class="btn"></div>
-        <div class="btn"></div>
+        <div class="btn" v-for="(btn,i) in item.btn" :key="i">{{btn}}</div>
       </div>
-      <div class="map"></div>
+      <div class="map">{{item.map}}</div>
     </div>
     <div class="bottom"></div>
   </div>
@@ -31,7 +24,22 @@
 export default {
   data() {
     return {
-      
+      location:{
+        bannerImg: '',
+        introduce: '',
+        locMap: '',
+        traffic:[
+          {
+            title: '',
+            btn: [],
+            map: ''
+          },{
+            title: '',
+            btn: [],
+            map: ''
+          }
+        ]
+      }
     }
   },
   computed: {
