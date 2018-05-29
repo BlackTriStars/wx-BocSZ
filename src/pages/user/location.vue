5031d8a19ee2fb7c26bd3e0d915ee08dd5996344
Liang Bing <bing_liang@17english.com>
2018-05-29 10:34:14 +0800

    lb

---
+++ src/pages/user/location.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="location">
+    <div class="banner">
+    </div>
+    <div class="introduce">
+
+    </div>
+    <div class="loc"></div>
+    <div class="traffic">
+      <div class="title"></div>
+      <div class="trafficBtn">
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+      <div class="map"></div>
+    </div>
+    <div class="traffic">
+      <div class="title"></div>
+      <div class="trafficBtn">
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+      <div class="map"></div>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    
+  },
+  components: {
+    
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+<style lang='less' rel='stylesheet/less'>
+  
+</style>
